5263bfc8483f4356be60c4e00b851c21a6673a71
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-07-18 16:40:26 +0530

    Add custom theme and logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,6 +2,9 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
 	const { children } = props;
@@ -16,12 +19,29 @@ function ElevationScroll(props) {
 	});
 }
 
+const useStyles = makeStyles((theme) => ({
+	toolbarMargin: {
+		...theme.mixins.toolbar,
+		marginBottom: "3em",
+	},
+	logo: {
+		height: "7em",
+	},
+}));
+
 export default function Header(props) {
+	const classes = useStyles();
+
 	return (
-		<ElevationScroll>
-			<AppBar position='fixed' color='primary'>
-				<Toolbar>Arc Development</Toolbar>
-			</AppBar>
-		</ElevationScroll>
+		<React.Fragment>
+			<ElevationScroll>
+				<AppBar position='fixed' color='primary'>
+					<Toolbar disableGutters>
+						<img src={logo} className={classes.logo} alt='company logo' />
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+			<div className={classes.toolbarMargin} />
+		</React.Fragment>
 	);
 }
