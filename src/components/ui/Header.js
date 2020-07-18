fae5e7bd9066ebb75b86f82c117a09f1864d9444
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-07-18 23:36:55 +0530

    Header design modifications

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
@@ -6,6 +6,7 @@ import { makeStyles } from "@material-ui/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo.svg";
 
@@ -27,8 +28,14 @@ const useStyles = makeStyles((theme) => ({
 		...theme.mixins.toolbar,
 		marginBottom: "3em",
 	},
+	logoContainer: {
+		padding: 0,
+		"&:hover": {
+			backgroundColor: "transparent",
+		},
+	},
 	logo: {
-		height: "7em",
+		height: "8em",
 	},
 	tabContainer: {
 		marginLeft: "auto",
@@ -49,19 +56,69 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header(props) {
 	const classes = useStyles();
+	const [value, setValue] = useState(0);
+
+	const handleChange = (e, value) => {
+		setValue(value);
+	};
+
+	useEffect(() => {
+		if (window.location.pathname === "/" && value !== 0) {
+			setValue(0);
+		} else if (window.location.pathname === "/services" && value !== 1) {
+			setValue(1);
+		} else if (window.location.pathname === "/revolution" && value !== 2) {
+			setValue(2);
+		} else if (window.location.pathname === "/about" && value !== 3) {
+			setValue(3);
+		} else if (window.location.pathname === "/contact" && value !== 4) {
+			setValue(4);
+		}
+	}, []);
 
 	return (
 		<React.Fragment>
 			<ElevationScroll>
 				<AppBar position='fixed' color='primary'>
 					<Toolbar disableGutters>
-						<img src={logo} className={classes.logo} alt='company logo' />
-						<Tabs className={classes.tabContainer}>
-							<Tab className={classes.tab} label='Home'></Tab>
-							<Tab className={classes.tab} label='Services'></Tab>
-							<Tab className={classes.tab} label='The Revolution'></Tab>
-							<Tab className={classes.tab} label='About Us'></Tab>
-							<Tab className={classes.tab} label='Contact Us'></Tab>
+						<Button
+							component={Link}
+							to='/'
+							className={classes.logoContainer}
+							onClick={() => setValue(0)}
+							disableRipple>
+							<img src={logo} className={classes.logo} alt='company logo' />
+						</Button>
+						<Tabs
+							className={classes.tabContainer}
+							value={value}
+							onChange={handleChange}
+							indicatorColor='primary'>
+							<Tab
+								className={classes.tab}
+								label='Home'
+								component={Link}
+								to='/'></Tab>
+							<Tab
+								className={classes.tab}
+								label='Services'
+								component={Link}
+								to='/services'></Tab>
+							<Tab
+								className={classes.tab}
+								label='The Revolution'
+								component={Link}
+								to='/revolution'></Tab>
+							<Tab
+								className={classes.tab}
+								label='About Us'
+								component={Link}
+								to='/about'></Tab>
+							<Tab
+								className={classes.tab}
+								label='Contact Us'
+								component={Link}
+								to='/contact'></Tab>
 						</Tabs>
 						<Button
 							variant='contained'
